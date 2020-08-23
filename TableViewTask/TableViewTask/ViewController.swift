4051e3ac3174ffc63d9643123c8948172701a409
Jitendra Kumar <Jitendra.kumar5@publicissapient.com>
2020-08-23 17:18:15 +0530

    TableViewTask Project has been created

---
+++ TableViewTask/TableViewTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewTask
+//
+//  Created by Jitendra Kumar on 23/08/20.
+//  Copyright © 2020 iOS developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
