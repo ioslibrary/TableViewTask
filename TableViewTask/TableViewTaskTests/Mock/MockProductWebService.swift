b009301f6bd1d9e0263e2d9d14790126121c47cc
Jitendra Kumar <Jitendra.kumar5@publicissapient.com>
2020-08-24 09:31:59 +0530

    Unit test cases classes added

---
+++ TableViewTask/TableViewTaskTests/Mock/MockProductWebService.swift
@@ -0,0 +1,23 @@
+
+import Foundation
+
+@testable import TableViewTask
+
+
+class MockProductWebService {
+ 
+    //MARK:  class properties
+    var isProductMethodCalled: Bool = false
+    var shouldReturnError: Bool = false
+ 
+    // Mock the api to get product data from server
+    func getProducts(completion: @escaping (ProductResult?, ProductAPIError?) -> (Void)) {
+        isProductMethodCalled = true
+        if shouldReturnError {
+            completion(nil, ProductAPIError.failedRequest(description: "Product request was not successful"))
+        } else {
+            let responseModel = ProductResult(title: "Some Title", products: [Product(title: "Any Title", imageURL: "Some URL", description: "Some Description")])
+            completion(responseModel, nil)
+        }
+    }
+}
